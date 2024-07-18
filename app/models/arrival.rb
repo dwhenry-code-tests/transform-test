437c76864534a63736d80201e8a1178d62cc94b0
David Henry <david@decoybecoy.com>
2024-07-18 21:35:14 +0100

    Improve UI at the cost of test coverage

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -6,13 +6,15 @@ class Arrival
   attribute :platform
   attribute :line_name
   attribute :destination
+  attribute :line_id
 
   def self.build(data)
     new(
       time_to_station: data['timeToStation'],
       platform: data['platformName'],
       line_name: data['lineName'],
-      destination: data['destinationName']
+      destination: data['destinationName'],
+      line_id: data['lineId']
     )
   end
 end
--- spec/system/arrivals_spec.rb
+++ spec/system/arrivals_spec.rb
@@ -14,7 +14,9 @@ RSpec.describe 'Arrivals' do
   end
 
   it 'can view a list of arrivals' do
-    visit arrivals_path
+    travel_to(Time.new(2024, 7, 18, 21, 30, 05)) do
+      visit arrivals_path
+    end
 
     within('table#arrivals thead') do
       expect(page).to have_content(
@@ -27,9 +29,9 @@ RSpec.describe 'Arrivals' do
 
     within('table#arrivals tbody') do
       expect(page).to have_content(
-        "Circle Edgware Road (Circle Line) Underground Station Eastbound - Platform 2 409 409\n" \
-        "Circle Hammersmith (H&C Line) Underground Station Westbound - Platform 1 509 509\n" \
-        "Circle Edgware Road (Circle Line) Underground Station Eastbound - Platform 2 609 609"
+        "Circle Edgware Road (Circle Line) Underground Station Eastbound - Platform 2 6:49 20:36:54\n" \
+        "Circle Hammersmith (H&C Line) Underground Station Westbound - Platform 1 8:29 20:38:34\n" \
+        "Circle Edgware Road (Circle Line) Underground Station Eastbound - Platform 2 10:09 20:40:14"
       )
     end
   end
